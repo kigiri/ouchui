caddf0ce7cc76fcff62053ad79a8b5e8ec0570ef
Clément <le.mikmac@gmail.com>
2018-07-26 14:21:14 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   state = {
     ip:"127.0.0.1",
     ville:"Load...",
-    codePostal:null,
+    codePostal:undefined,
     ext:undefined,
     time:Date.now()
   }
@@ -22,20 +22,16 @@ class App extends Component {
       .then(req => req.json())
       .then(res => {
         this.setState({ip:res.ip})
+        return res.ip
       })
-      .then( res => {
-        fetch(`http://api.ipstack.com/88.186.169.153?access_key=b908dcc9061f0b9644498ae6329f7c3a`)
-        .then(req => req.json())
-        .then(res => {
-          this.setState({ville:res.city})
-          this.setState({codePostal:res.zip})
-          fetch(`https://api.openweathermap.org/data/2.5/weather?q=${res.city}&units=metric&APPID=339ca6935cce3023e268810a00f76910`)
-            .then(req => req.json())
-            .then(res => {
-             this.setState({ext:res})
-            })
-        })
-      })
+      .then(ip => fetch(`http://api.ipstack.com/${ip}?access_key=b908dcc9061f0b9644498ae6329f7c3a`))
+      .then(req => req.json())
+      .then(res => {
+        this.setState({ville:res.city, codePostal:res.zip})
+        return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${res.city}&units=metric&APPID=339ca6935cce3023e268810a00f76910`))
+      .then(req => req.json())
+      .then(res => this.setState({ext:res}))
+    })
   }
 
   render() {
