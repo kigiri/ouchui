bff29d7e79d0d944ce0a4cef7f3cd74d8064c949
Clément <le.mikmac@gmail.com>
2018-07-26 14:32:15 +0200

    Update information.js

--- src/Components/information.js
+++ src/Components/information.js
@@ -1,14 +1,28 @@
 import React from 'react'
 
+import CircularProgress from '@material-ui/core/CircularProgress'
 import Who from './who.js'
 import Fiche from './fiche.js'
 
 // codePostal, ville, ip, ext
 const Information = ({props}) => {
+  const content = props.ext
+    ? <Fiche
+        codePostal={props.codePostal}
+        ville={props.ville}
+        meteoTemp={props.ext.main.temp}
+        meteoDes={props.ext.weather[0].description}
+        meteoImg={props.ext.weather[0].icon}
+        temp={props.ext.main.temp}
+      />
+    : <div>
+        <h6 className='row max' >Environement</h6>
+         <CircularProgress mode="indeterminate" color="#ffcc00"  size={10} />
+      </div>
   return (
     <div className='box'>
       <div className='boxRight' >
-        <Fiche ville={props.ville} ext={props.ext} codePostal={props.codePostal} />
+        {content}
       </div>
       <div className='boxLeft'>
        <Who  ip={props.ip} />
